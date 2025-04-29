ac392af94561cf0807ecec6652784663fc0919da
Johnsonchy91 <105237048+Johnsonchy91@users.noreply.github.com>
2025-04-29 02:06:06 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1420,7 +1420,7 @@ with tab7:
     
     with st.expander("Story 3: My Sister's Keeper"):
         st.markdown(
-            ""Morgan and Vanessa, I walked this evening. First chance I've had in a while.
+            """Morgan and Vanessa, I walked this evening. First chance I've had in a while.
             And I talked on the phone to a friend of mine who was also walking at the time and had not walked in a while. 
             I invited her to walk with me and told her about Harriet Day and the meeting last night. 
             I also shared GirlTREK information with her and invited her to join. We're going to start walking together!
